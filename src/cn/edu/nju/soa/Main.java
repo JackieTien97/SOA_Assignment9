b7fde89a6233ba230d4c7b5e9a54c43bff7cbd2c
Slow_Time <1395881075@qq.com>
2018-03-06 18:42:17 +0800

    aa

--- src/cn/edu/nju/soa/Main.java
+++ src/cn/edu/nju/soa/Main.java
@@ -1,15 +1,20 @@
 package cn.edu.nju.soa;
 
-import cn.edu.nju.soa.model.StudentList;
 import cn.edu.nju.soa.transform.Generator;
+import cn.edu.nju.soa.transform.XmlParser;
 
-import java.io.FileNotFoundException;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            StudentList studentList = Generator.generateData();
-        } catch (FileNotFoundException e) {
+            OutputStream outputStream = new FileOutputStream(new File("doc/文档2.xml"));
+            String result = XmlParser.marshal(Generator.generateData());
+            outputStream.write(result.getBytes());
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
